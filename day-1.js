99e5fd36e53d30d4269443ee0be1c29787594e81
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-04-07 09:49:56 +0530

    Create day-1.js
    Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down.

---
+++ day-1.js
@@ -0,0 +1,9 @@
+// rounded down the digit By using Math.floor & Math.round() 
+
+
+function roundedDivison(a,b){
+    console.log(Math.floor(a/b))
+    console.log(Math.round(b/a))
+}
+
+roundedDivison(8,3)
