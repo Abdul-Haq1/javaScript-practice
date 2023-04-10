cfc2217dcd48599f64df4757c309e9dbeaf9b131
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-04-10 15:38:37 +0530

    Convert Age to Days

---
+++ day-8.js
@@ -0,0 +1,7 @@
+//  Use 365 days as the length of a year for this challenge.
+//  Ignore leap years and days between last birthday and now.
+//  Expect only positive integer inputs.
+
+function calcAge(age) {
+	return age*365;
+}
