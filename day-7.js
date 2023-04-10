4b9891fc7d26d89e7d44dc6502742bdf0c817ac3
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-04-10 15:35:28 +0530

    Maximum Edge of a Triangle

---
+++ day-7.js
@@ -0,0 +1,6 @@
+//  (side1 + side2) - 1 = maximum range of third edge.
+//  The side lengths of the triangle are positive integers.
+
+function nextMaxEdge(side1, side2) {
+	return side1+side2-1;
+}
