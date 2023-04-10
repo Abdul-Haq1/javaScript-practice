dfe04b3aee5ac545eec860c6eb7853f4c9857559
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-04-10 16:06:44 +0530

    maximum range of third edge

---
+++ day11.js
@@ -0,0 +1,7 @@
+// array elements can be accessed by using array indexing
+
+// Bracket notation: You can use square brackets to access the elements of an array by their index. The index starts at 0 for the first element,
+  // and increases by 1 for each subsequent element. For example, myArray[0] would return the first element of the array.
+function getFirstValue(arr) {
+	return arr[0];
+}
