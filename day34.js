5d01f1bb571779b9b991b9875dda46e7df541236
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-04-30 17:39:34 +0530

    Return Early Pattern for Functions
    When a return statement is reached, the execution of the current function stops and control returns to the calling location.

---
+++ day34.js
@@ -0,0 +1,9 @@
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+
+// The above will display the string Hello in the console, and return the string World. 
+// The string byebye will never display in the console, because the function exits at the return statement.
