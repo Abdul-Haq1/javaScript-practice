1633b50e9812268bc8d5ee4d1e0e0fe5b3e2c2b3
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-01 23:53:32 +0530

    Return Early Pattern for Functions

---
+++ day35.js
@@ -0,0 +1,20 @@
+// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun(); 
+// When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
