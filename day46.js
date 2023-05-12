8853176f87e99d1ef8ed1354ab83a8245dce747a
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-12 17:58:08 +0530

    Iterate Through an Array with a For Loop

---
+++ day46.js
@@ -0,0 +1,8 @@
+
+//A common task in JavaScript is to iterate through the contents of an array. One way to do that is with a for loop. This code will output each element of the array arr to the console:
+
+const arr = [10, 9, 8, 7, 6];
+
+for (let i = 0; i < arr.length; i++) {
+   console.log(arr[i]);
+}
