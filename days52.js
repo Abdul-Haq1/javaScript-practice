755ac4b1ccaf5586401f8b6829c3bc1a707ec4b6
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-15 22:57:12 +0530

    Use Arrow Functions to Write Concise Anonymous Functions

---
+++ days52.js
@@ -0,0 +1,11 @@
+const myFunc = function() {
+  const myVar = "value";
+  return myVar;
+}
+
+// ES6 provides us with the syntactic sugar to not have to write anonymous functions this way. Instead, you can use arrow function syntax:
+
+const myFunc = () => {
+  const myVar = "value";
+  return myVar;
+}
