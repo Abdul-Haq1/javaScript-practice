410f0c7e923324ca404482b7e9d9c0a95d5d8307
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-17 23:35:10 +0530

    Destructuring Assignment to Assign Variables from Objects

---
+++ day55.js
@@ -0,0 +1,14 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+// const highToday = HIGH_TEMPERATURES.today;
+// const highTomorrow = HIGH_TEMPERATURES.tomorrow; 
+
+// Destructuring Assignment to Assign Variables from Objects
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES
+// Only change code above this line
