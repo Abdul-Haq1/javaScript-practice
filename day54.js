d195af6f1fda049f42d485ca76cc3176b0f54bce
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-17 23:24:43 +0530

    Destructuring Assignment to Extract Values from Objects

---
+++ day54.js
@@ -0,0 +1,15 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+// above line can be write like this, method called- Destructuring Assignment to Extract Values from Objects
+const {today,tomorrow} = HIGH_TEMPERATURES
+
+// Only change code above this line
