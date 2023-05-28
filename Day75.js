19bc6cb33b602c84e1003a2fda6a159d44c15543
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-05-28 23:57:45 +0530

    Create Day75.js

---
+++ Day75.js
@@ -0,0 +1,20 @@
+//The RegExp Object
+//A regular expression is a pattern of characters.
+
+//The pattern is used for searching and replacing characters in strings.
+
+//The RegExp Object is a regular expression with added Properties and Methods.
+
+//Modifier	Description
+//g	Perform a global match (find all matches rather than stopping after the first match)
+//i	Perform case-insensitive matching
+//m	Perform multiline matching
+//Brackets
+//Brackets are used to find a range of characters:
+
+//Expression	Description
+//[abc]	Find any character between the brackets
+//[^abc]	Find any character NOT between the brackets
+//[0-9]	Find any character between the brackets (any digit)
+//[^0-9]	Find any character NOT between the brackets (any non-digit)
+//(x|y)	Find any of the alternatives specified
