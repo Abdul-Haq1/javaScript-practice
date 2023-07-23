cabdd03042b8da4f3bf17649ecc6257cb472b2c3
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-07-23 23:25:07 +0530

    Find the Longest Word in a String

---
+++ day150.js
@@ -0,0 +1,18 @@
+//Find the Longest Word in a String
+
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+//console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology"))
+
