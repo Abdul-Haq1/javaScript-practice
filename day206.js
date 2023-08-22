16a1cf472f6322120b5119fe56b1d092e1bbc730
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-08-22 17:05:40 +0530

    Use Inheritance So You Don't Repeat Yourself

---
+++ day206.js
@@ -0,0 +1,28 @@
+
+// There's a principle in programming called Don't Repeat Yourself (DRY). 
+// The reason repeated code is a problem is because any change requires fixing code in multiple places. 
+// This usually means more work for programmers and more room for errors.
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
