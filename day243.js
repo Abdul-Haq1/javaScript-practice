d3fed650a1f618094c60d9146a055e5b3b1fb0a4
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2023-09-01 14:31:23 +0530

    Spinal Tap Case

---
+++ day243.js
@@ -0,0 +1,7 @@
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+  // Replace space and underscore with -
+  return str.replace(regex, "-").toLowerCase();
