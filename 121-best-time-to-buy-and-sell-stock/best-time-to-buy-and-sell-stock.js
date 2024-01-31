31341b4363fc977abf69bf56cc04cc49c801f6fc
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2024-01-31 23:28:37 +0530

    Time: 95 ms (13.27%) | Memory: 58.5 MB (19.81%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+    if (!prices || prices.length < 2) {
+        return 0;
+    }
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        minPrice = Math.min(minPrice, prices[i]);
+    }
+
+    return maxProfit;
+}
