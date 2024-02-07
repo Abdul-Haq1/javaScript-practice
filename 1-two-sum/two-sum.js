0efba39cb0e757c8171e2c72d9a2aded57543d65
Abdul Haq <78525870+Abdul-Haq1@users.noreply.github.com>
2024-02-07 19:50:56 +0530

    Time: 85 ms (46.54%) | Memory: 55.5 MB (5.48%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,43 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
 
-    for(let j=0; j < nums.length; j++){
-        for(let i=j+1; i < nums.length; i++){
-            if ((nums[i]+ nums[j]) === target){
-                return [j ,i];
+//     for(let j=0; j < nums.length; j++){
+//         for(let i=j+1; i < nums.length; i++){
+//             if ((nums[i]+ nums[j]) === target){
+//                 return [j ,i];
                 
-            }
-        }        
+//             }
+//         }        
+//     }
+// };
+
+function twoSum(nums, target) {
+    let numsWithIndices = nums.map((num, index) => ({ num, index }));
+
+    numsWithIndices.sort((a, b) => a.num - b.num);
+    console.log(numsWithIndices)
+    // Initialize left and right pointers
+    let left = 0;
+    let right = nums.length - 1;
+
+    // Iterate until left and right pointers meet
+    while (left < right) {
+        let currentSum = numsWithIndices[left].num + numsWithIndices[right].num;
+
+        if (currentSum === target) {
+            return [numsWithIndices[left].index, numsWithIndices[right].index];
+        }
+        // If the current sum is less than the target, move the left pointer to the right
+        else if (currentSum < target) {
+            left++;
+        }
+        // If the current sum is greater than the target, move the right pointer to the left
+        else {
+            right--;
+        }
     }
-};
+    // If no such pair is found, return an empty array
+    return [];
+}
